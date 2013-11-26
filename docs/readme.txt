de5aa520c4db87591450ffa0305065dbdb3f63f4
George Spiceland <georgespiceland@gmail.com>
2013-11-26 00:10:52 -0500

    Updated login screen. -Now uses hashes to store username and password combinations! -Now functions correctly, and can write/retrieve login info! Updated readme.txt -Fixed minor typographical error. Updated todo.txt -Updated status of Login screen module from Pending to First Draft

--- docs/readme.txt
+++ docs/readme.txt
@@ -2,7 +2,7 @@ brownfield-mccoy
 =========================
 A Tachyon Digital project
 =========================
-11.23.2013
+11.26.2013
 
 A Learning Lab environment for programming via the creation of a game involving using tools to break into
 encrypted networks.
@@ -17,7 +17,7 @@ Game will be engineered to use and develop the following skill sets of players w
   -Project will sometimes involve choices made during timed situations where rapid decision making using provided
   information will be critical to success.
 4.) Ideas for Network types
-  -Make it so certian networks have certain password types. Simpler networks may only have alpha-numeric passwords while
+  -Make it so certain networks have certain password types. Simpler networks may only have alpha-numeric passwords while
   more heavily encrypted networks need specific characters and lengths.
   -Some networks may use public/private keys to access instead, requiring the player to develop a method of retrieving
   a public key to gain access to the network.
--- intro2.py
+++ intro2.py
@@ -1,59 +1,48 @@
 __author__ = 'George Spiceland'
+#Brownfield-McCoy is a game of skill based on trying to think through often complicated and multi-layered problems
+#in an attempt to access secured networks. It is also a training field for Python for the Tachyon-Digital team.
 
 #This Py is a beginning and introductory Py to cover basic functions and terms for use with Python 3.3.2.
+import hashlib
+import getpass
 
 
-#A basic function for determing if users are new users or returning users.
+#A basic function for determining if users are new users or returning users.
 def login_menu():
     #Just an interesting introduction here that explains the options.
     print('Welcome to Brownfield-McCoy\n' 'This is a game of skill\n')
-    print('If you have played before, choose 1.) Existing User\n'
-          'If you have never played before, choose 2.) New User\n')
+    print("Tell me if you are a [new] player, or a [returning] one.\n")
     while True:
         #While loop allows for us to force players to answer correctly.
-        next = input('Have you played before? >')
-        if next == '1':
-            #If statement here checks if players are returning...
-            print('A returning player! Excellent.')
-            return 'old'
-        elif next == '2':
-            #Else-if statement here checks if players are new.
-            print ('A new player, how wonderful.')
+        next_act = input('Have you played before? >')
+        #If statement here checks if players are returning...
+        if next_act in ('n', 'ne', 'new', 'N', 'NE', 'NEW', 'Ne', 'New'):
             return 'new'
+        elif next_act in ('r', 're', 'ret', 'retu', 'retur', 'return', 'returni', 'returnin', 'returning', 'R',
+                          'RETURNING'):
+            return 'old'
         else:
-            #Else statement here re-runs the loop and gives an error code.
-            print("I'm sorry, but that isn't an option.")
+            print("Sorry, I don't understand.")
 
 
-#A basic function for requesting the user's name.
-def ask_name(prompt, min_length, max_length, complaint):
-    #While loop waits for user to input a name with proper length.
+#A basic function for requesting the user's information.
+def ask_info(prompt, min_length, max_length, complaint, password):
+    #While loop waits for the user to input the requested information.
     while True:
-        usr_name = input(prompt)
-        if min_length <= len(usr_name) <= max_length:
-            #If statement tests whether or not name is usable length.
-            print('Thank you, {}'.format(usr_name))
-            return usr_name
+        if password == 'y':
+            usr_info = getpass.getpass(prompt)
         else:
-            #Else statement asks user to re-enter username to where it's correct length.
+            usr_info = input(prompt)
+        if min_length <= len(usr_info) <= max_length:
+            #If statement tests whether or the the info is usable length.
+            print('Thank you.')
+            return usr_info
+        else:
+            #Else statement asks user to re-enter info to where it's correct length.
             print(complaint)
 
 
-#A basic function for requesting the user's password.
-def ask_pass(prompt, min_length, max_length, complaint):
-    #While loop waits for user to input a password with proper length.
-    while True:
-        usr_pass = input(prompt)
-        if min_length <= len(usr_pass) <= max_length:
-            #If statement tests whether or not password is usable length.
-            print('You will use {} as your password.'.format(usr_pass))
-            return usr_pass
-        else:
-            #Else statement asks user to re-enter password to where it's correct length.
-            print(complaint)
-
-
-#Function writes information to a file. Currently can only write info to target file
+#Function writes information to a file. Currently can only write info to target file.
 def write2file(info_written, target_file, newline):
     #With statement opens file as [f] for use inside function. Automatically closes file.
     with open(target_file, 'a') as f:
@@ -64,27 +53,59 @@ def write2file(info_written, target_file, newline):
             f.write('\n')
         else:
             f.write(' ')
-    print('File Written Successfully!')
 
 
+#Function reads information from a file. Currently can only read info from target file.
+def read0file(target_file):
+    #With statement opens file as [f] for use inside function. Automatically closes file.
+    with open(target_file, 'r') as f:
+        #Read statement reads file.
+        temp_nfo = f.read()
+    return temp_nfo
+
+
+#Function uses ask_info function to get new user's name and password.
 def new_player_get():
+    #Prints the account creation intro.
     print("Now we'll set you up with an account.")
-    usr_name = ask_name("What's your name, new user? >", 6, 20, "Must be between 6 and 20 characters!")
-    usr_pass = ask_pass("What's your password, new user? >", 6, 20, "Must be between 6 and 20 characters!")
-    write2file(usr_name,'files/credentials.txt','0')
-    write2file(usr_pass,'files/credentials.txt','n')
-    return usr_name
+    #Calls the usr_info function.
+    usr_n = ask_info("What's your name, new user? >", 6, 20, "Must be between 6 and 20 characters!", 'n')
+    usr_p = ask_info("What's your password, new user? >", 6, 20, "Must be between 6 and 20 characters!", 'y')
+    #Creates a hash using sha256 encoding.
+    h = hashlib.sha256()
+    #Updates the hash using the user's password and then user's name.
+    h.update(usr_p.encode(encoding='utf-8', errors='strict') + usr_n.encode(encoding='utf-8', errors='strict'))
+    usr_h = h.hexdigest()
+    #Writes the hash to the credentials file, including the newline.
+    write2file(usr_h, 'files/credentials.txt', 'n')
+    return usr_n
 
+
+#Function uses ask_info function to get returning user's name and password.
 def old_player_get():
-    pass
+    #Prints the returning user intro.
+    print("Welcome back, Enter your credentials to log in.")
+    #Calls the usr_info function.
+    while True:
+        usr_n = ask_info("Provide your login name. >", 6, 20, "Must be between 6 and 20 characters!", 'n')
+        usr_p = ask_info("Provide your login password. >", 6, 20, "Must be between 6 and 20 characters!", 'y')
+        #Creates a hash using sha256 encoding.
+        h = hashlib.sha256()
+        #Updates the hash using the user's password and then user's name.
+        h.update(usr_p.encode(encoding='utf-8', errors='strict') + usr_n.encode(encoding='utf-8', errors='strict'))
+        usr_h = h.hexdigest()
+        #Checks the info against the credentials file.
+        temp_info = read0file("files/credentials.txt")
+        if usr_h in temp_info:
+            print('Welcome back ', usr_n)
+            return usr_n
+        else:
+            print("Sorry, I can't find your records, try again.")
 
 
 user_type = login_menu()
 if user_type == 'new':
     active_player = new_player_get()
 else:
-    active_player = 'old'
-print(active_player)
-
-#print(usr_name)
-#print(usr_pass)
+    active_player = old_player_get()
+print("Successfully logged you in, ", active_player, ".")
